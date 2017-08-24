f87dfb19ef0f9df390e838e3dc2e7a2517ee40ba
Shawn Allen <shawn.allen@github.com>
2017-08-24 16:05:38 -0700

    ensure that output files exist

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,12 @@
+const fs = require("fs")
 const test = require("ava")
 const build = require("../lib/build.js")
 
 test("builds test css", t => {
-  try {
-    build("./tests/test.scss", {})
-  } catch (e) {
-    t.fail(e.message)
-  }
+  return build("./tests/test.scss", {})
+    .then(output => {
+      Object.entries(output).forEach(([key, file]) => {
+        t.is(fs.existsSync(file), true, `No ${key} output: ${file}`)
+      })
+    })
 })
