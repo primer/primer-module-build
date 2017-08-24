d18d57497d41eb1eaa83f704fbb54328d6d391b9
Shawn Allen <shawn.allen@github.com>
2017-08-24 16:58:44 -0700

    don't use Object.entries()

--- tests/index.js
+++ tests/index.js
@@ -5,8 +5,8 @@ const build = require("../lib/build.js")
 test("builds test css", t => {
   return build("./tests/test.scss", {})
     .then(output => {
-      Object.entries(output).forEach(([key, file]) => {
-        t.is(fs.existsSync(file), true, `No ${key} output: ${file}`)
+      Object.keys(output).forEach(key => {
+        t.is(fs.existsSync(output[key]), true, `No ${key} output: ${output[key]}`)
       })
     })
 })
