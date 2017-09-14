03e4ea8fad55b2bf7b0f60dede74530f53b14274
Jon Rohan <rohan@github.com>
2017-09-13 19:04:08 -0700

    tesT

--- tests/index.js
+++ tests/index.js
@@ -5,6 +5,8 @@ const build = require("../lib/build.js")
 test("builds test css", t => {
   return build("./tests/test.scss", {})
     .then(output => {
+      /*eslint-disable*/
+      console.log(fs.readdirSync(process.cwd()));
       Object.keys(output).forEach(key => {
         t.is(fs.existsSync(output[key]), true, `No ${key} output: ${output[key]}`)
       })
